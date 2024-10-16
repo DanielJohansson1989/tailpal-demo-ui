d320c458c329566b5552f6065fee6472720a3171
Sjohansson88 <sjohansson88@hotmail.com>
2024-10-16 11:28:16 +0200

    Dashboard & BankId

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
 import './App.css';
+import BankID from './bankId';
+import Dashboard from './Dashboard';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <span>        
-          <img src="https://static.wixstatic.com/media/bb9651_c18982779dea4131bb1d1cd239a30924~mv2.png/v1/fill/w_80,h_80,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/TailPal_Black_green.png" className="tailpal-logo" alt="logo" />
-        </span>
-        <span className='timeContainer'>10:00</span>
-        <span>
-          <i className='fa-solid fa-bars' id='hamburgerIcon'></i>
-        </span>
-      </header>
-      <main>
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        <Header /> 
+        <main>
+          <Routes>
+            <Route path="/" element={<Dashboard />} /> 
+            <Route path="/bankid" element={<BankID />} /> 
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+}
+
+function Header() {
+  const navigate = useNavigate(); 
+
+  const handleLogoClick = () => {
+    navigate('/'); 
+  };
+
+  return (
+    <header className="App-header">
+      <span onClick={handleLogoClick} style={{ cursor: 'pointer' }}>        
+        <img 
+          src="https://static.wixstatic.com/media/bb9651_c18982779dea4131bb1d1cd239a30924~mv2.png/v1/fill/w_80,h_80,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/TailPal_Black_green.png" 
+          className="tailpal-logo" 
+          alt="logo" 
+        />
+      </span>
+      <span className='timeContainer'>10:00</span>
+      <span>
+        <i className='fa-solid fa-bars' id='hamburgerIcon'></i>
+      </span>
+    </header>
   );
 }
 
